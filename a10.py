8463e6a80580cf6ec05012451d6af5ce17863832
Franco Pena-Lepe <fpena-lepe@cps.edu>
2026-05-08 10:55:06 -0500

    did get population

--- a10.py
+++ a10.py
@@ -9,7 +9,7 @@ import requests
 
 def get_page_html(title: str) -> str:
     response = requests.get(
-        "https://en.wikipedia.org/w/api.php"
+        "https://en.wikipedia.org/w/api.php",
         params={
             "action": "parse",
             "page": title,
@@ -22,12 +22,13 @@ def get_page_html(title: str) -> str:
     return data["parse"]["text"]["*"]
 
 
+
 def get_first_infobox_text(html: str) -> str:
     """Gets first infobox html from a Wikipedia page (summary box)
 
     Args:
         html - the full html of the page
-    
+
     Returns:
         html of just the first infobox
     """
@@ -112,6 +113,15 @@ def get_birth_date(name: str) -> str:
 
     return match.group("birth")
 
+def get_population(name: str) -> str:
+    
+    infobox_text = clean_text(get_first_infobox_text(get_page_html(name)))
+
+    pattern = r"Population.*?(?P<pop>\d{1,3}(?:,\d{3})+)"
+    error_text = "Page infobox has no population information"
+
+    match = get_match(infobox_text, pattern, error_text)
+    return match.group("pop")
 
 # below are a set of actions. Each takes a list argument and returns a list of answers
 # according to the action and the argument. It is important that each function returns a
@@ -141,6 +151,8 @@ def polar_radius(matches: List[str]) -> List[str]:
     """
     return [get_polar_radius(matches[0])]
 
+def population(matches: List[str]) -> List[str]:
+    return [get_population(" ".join(matches))]
 
 # dummy argument is ignored and doesn't matter
 def bye_action(dummy: List[str]) -> None:
@@ -157,6 +169,8 @@ Action = Callable[[List[str]], List[Any]]
 pa_list: List[Tuple[Pattern, Action]] = [
     ("when was % born".split(), birth_date),
     ("what is the polar radius of %".split(), polar_radius),
+    ("what is the population of %".split(), population),
+    ("population of %".split(), population),
     (["bye"], bye_action),
 ]
 
